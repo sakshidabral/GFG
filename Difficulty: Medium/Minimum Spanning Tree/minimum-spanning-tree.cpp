514b72c4a404b16488ad1fdc12aeb667f241e2cb
sakshidabral <125342226+sakshidabral@users.noreply.github.com>
2025-08-07 20:09:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -1,36 +1,52 @@
 class Solution {
   public:
-    int spanningTree(int V, vector<vector<int>>& edges) {
-        // code here
-        vector<vector<pair<int, int>>> adj(V);
-        for (auto &edge : edges) {
-            int u = edge[0], v = edge[1], wt = edge[2];
-            adj[u].push_back({v, wt});
-            adj[v].push_back({u, wt});
+    int find(int i, vector<int>& parent){
+        if(i == parent[i]){
+            return i;
         }
         
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        vector<bool> vis(V, false);
+        return parent[i] = find(parent[i], parent);
+    }
+    
+    void unite(int x, int y, vector<int>& parent, vector<int>& rank){
+        int xp = find(x, parent);
+        int yp = find(y, parent);
+        
+        if(xp == yp) return;
+        
+        if(rank[xp] < rank[yp]){
+            parent[xp] = yp;
+        }
+        else if(rank[yp] < rank[xp]){
+            parent[yp] = xp;
+        }
+        else{
+            parent[yp] = xp;
+            rank[xp]++;
+        }
+    }
+    
+    int spanningTree(int V, vector<vector<int>>& edges) {
+        // code here
+        sort(edges.begin(), edges.end(), [](vector<int>& a, vector<int>& b){
+            return a[2] < b[2]; // Sort by weight
+        });
+        
+        vector<int> parent(V),rank(V, 0);
+        for(int i=0;i<V;i++){
+            parent[i] = i;
+        }
         
         int sum = 0;
-        pq.push({0,0}); // {weight, node}
-        
-        while(!pq.empty()){
-            int wt = pq.top().first;
-            int u = pq.top().second;
-            pq.pop();
-            
-            if(vis[u] == true){
-                continue;
-            }
-            
-            vis[u] = true;
-            sum += wt;
-            
-            for(auto &[v, edgeWt] : adj[u]){
-                if(vis[v] == false){
-                    pq.push({edgeWt, v});
-                }
+        for (auto& edge : edges) {
+            int u = edge[0], v = edge[1], wt = edge[2];
+
+            int up = find(u, parent);
+            int vp = find(v, parent);
+
+            if (up != vp) {
+                unite(up, vp, parent, rank);
+                sum += wt;
             }
         }
         
